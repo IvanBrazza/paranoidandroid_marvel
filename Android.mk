080f389b19fa6d882bdd274c0ce713e3e5980bab
Ivan <dudeman1996@gmail.com>
2012-08-20 18:47:35 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,3 @@
-LOCAL_PATH := $(call my-dir)
-ifeq
-($(TARGET_BOOTLOADER_BOARD_NAME),marvel)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
-
-ifeq 
-($(TARGET_BOOTLOADER_BOARD_NAME),marvelc)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),marvel)
+    include $(call all-subdir-makefiles)
 endif
