5f6a39c9ee3ffc5c1b47bc55a1ff6bc4cda5ef84
Ivan Brazza <dudeman1996@gmail.com>
2012-08-30 15:46:37 +0100

    Updated for jellybean

--- cm.mk
+++ cm.mk
@@ -8,16 +8,15 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/marvel_pa/device_marvel.mk)
+$(call inherit-product, device/htc/marvel/device_marvel.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
 PRODUCT_NAME := cm_marvel
 PRODUCT_BRAND := htc_europe
-PRODUCT_MODEL := Wildfire S A510e
+PRODUCT_MODEL := Wildfire S
 PRODUCT_MANUFACTURER := HTC
-
-PRODUCT_VERSION_DEVICE_RELEASE := BETA1
+PRODUCT_VERSION_DEVICE_RELEASE := ALPHA2
 PRODUCT_VERSION_DEVICE_SPECIFIC := -PARODUDE-$(PRODUCT_VERSION_DEVICE_RELEASE)
 
 # CM_RELEASE := true
@@ -27,7 +26,7 @@ CM_EXTRAVERSION := PARODUDE-$(PRODUCT_VERSION_DEVICE_RELEASE)
 BUILD_ID := PARODUDE-$(PRODUCT_VERSION_DEVICE_RELEASE)-$(shell date -u +%d)-$(shell date -u +%m)-$(shell date -u +%Y)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.0.4/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="0.1 PARODUDE release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_marvel BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cm/htc_marvel/marvel:4.1.1/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="0.1 PARODUDE release-keys"
 
 # GooManager Stuffs
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -41,5 +40,3 @@ PRODUCT_COPY_FILES += \
 #ROM Manager Stuffs
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=dudeman1996
-
-
