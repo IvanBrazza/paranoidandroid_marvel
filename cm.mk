fa8c45b9f6492031dba31ba9108783432404ab40
Ivan <dudeman1996@gmail.com>
2012-11-07 22:55:44 +0000

    Remove CMFileManager

--- cm.mk
+++ cm.mk
@@ -45,7 +45,3 @@ PRODUCT_PROPERTY_OVERRIDES := \
     dalvik.vm.heapgrowthlimit=48m \
     dalvik.vm.heapsize=128m \
     dalvik.vm.dexopt-flags=m=y
-
-# Include CMFileManager
-PRODUCT_PACKAGES += \
-    CMFileManager
